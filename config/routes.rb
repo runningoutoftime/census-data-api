3b8c3042c6d8b18bbaf9f78acbf15c7bcd353f42
runningoutoftime <jfregoso17@gmail.com>
2018-11-21 22:18:34 -0800

    rename routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/import_tool' => 'code_based_statistical_areas#import_tool'
   post '/import' => 'code_based_statistical_areas#import', :format => :js
-  get '/census_data' =>  'code_based_statistical_areas#census_data_api', :format => :json
+  get '/look_up' =>  'code_based_statistical_areas#census_data_api', :format => :json
 end
